22e718281de5d34968223af42e6c2f0fc0de228e
Jawad Fahim <72640742+JawadFahim@users.noreply.github.com>
2025-02-20 21:58:00 +0600

    Update phpMailer.php

--- phpMailer.php
+++ phpMailer.php
@@ -14,12 +14,12 @@ function sendVerificationCode($email, $verification_code) {
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output ukfq ijsh ychd bvwr
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'ricefield136@gmail.com';                     //SMTP username
-        $mail->Password   = 'ukfq ijsh ychd bvwr';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
